23c78acb769b3074cd6106ac14c632ecac3f74bc
pane33 <120104302+pane33@users.noreply.github.com>
2023-01-18 01:12:56 +0100

    fatto

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -1,4 +1,8 @@
 class AboutController < ApplicationController
     def index
+        if !user_signed_in?
+            redirect_to login_path
+            flash.alert = "REGISTRATI O ACCEDI PER PROSEGUIRE AL PROGRAMMA"
+        end
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  
+  devise_for :user, :path => '', :path_names => { :sign_in => "login", :sign_out => "logout", :sign_up => "register" }
   root to: "home#index"
 
   get "about", to: "about#index"
